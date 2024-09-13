a577e41d46699baa3548f697afde9f5e8690a976
vivekvishwa011 <vishwavivek575@gmail.com>
2024-09-13 13:48:19 +0530

    ProfileLogo and creditCard

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,2 +1,7 @@
+import CreditCard from "../../assets/images/creditCard.png";
+import CompanyLogo from "../../assets/images/companyLogo.png";
+
 export const images = {
+  creditCard: CreditCard,
+  companyLogo: CompanyLogo,
 };
